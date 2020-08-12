199e247366dada0e123122c4b3e66ca015e09464
Mohit Awasthi <mohit.awasthi@healthsetgo.co.in>
2020-08-12 23:48:08 +0530

    Button Demo added

--- example/src/Containers/DocumentationButtonContainer/Component/DocumentationButton.js
+++ example/src/Containers/DocumentationButtonContainer/Component/DocumentationButton.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { createRef } from 'react';
 
 import ChildLayout from '../../../Components/ChildLayout';
 
 import './DocumentationButton.css';
 
-import META from '../source';
+import META, { OPTION_TYPE_BUTTON, OPTION_SIZE_BUTTON } from '../source';
 
 import { Button, FormElements } from 'react-blended-components';
 
@@ -14,61 +14,48 @@ class DocumentationButton extends React.Component {
 
     this.state = {
       type: 'PRIMARY',
+      size: 'NORMAL'
     };
   }
 
   renderDemo = () => (
     <div className="rbc-overview-button-demo-group">
-      <div className="col-lg-6 center">
-        <Button type={this.state.type}>Demo</Button>
+      <div className="col-lg-12 center">
+        <Button type={this.state.type} size={this.state.size}>Demo</Button>
       </div>
-      <div className="col-lg-6">
+    </div>
+  );
+
+  renderDemoOptions = () => (
+    <div className="rbc-overview-button-demo-group">
+      <div className="col-lg-12">
         <FormElements.Radio
           name="button-type"
           label="Type"
           onChange={(e) => {
-            this.setState({type: e})
+            this.setState({ type: e });
           }}
           value={this.state.type}
-          options={[
-            {
-              id: 'type-primary',
-              label: 'BASIC',
-              value: 'BASIC'
-            },
-            {
-              id: 'type-primary',
-              label: 'DEFAULT',
-              value: 'DEFAULT'
-            },
-            {
-              id: 'type-primary',
-              label: 'PRIMARY',
-              value: 'PRIMARY',
-            },
-            {
-              id: 'type-primary',
-              label: 'SUCCESS',
-              value: 'SUCCESS',
-            },
-            {
-              id: 'type-primary',
-              label: 'INFO',
-              value: 'INFO',
-            },
-            {
-              id: 'type-primary',
-              label: 'WARNING',
-              value: 'WARNING',
-            },
-          ]}
+          options={OPTION_TYPE_BUTTON}
+        />
+      </div>
+      <div className="col-lg-12">
+        <FormElements.Radio
+          name="button-size"
+          label="Size"
+          onChange={(e) => {
+            this.setState({ size: e });
+          }}
+          value={this.state.size}
+          options={OPTION_SIZE_BUTTON}
         />
       </div>
     </div>
   );
 
   render() {
-    return <ChildLayout meta={META(this.renderDemo)} />;
+    console.log(this.refButton);
+    return <ChildLayout meta={META(this.renderDemo, this.renderDemoOptions)} />;
   }
 }
 
--- example/src/Containers/DocumentationButtonContainer/source.js
+++ example/src/Containers/DocumentationButtonContainer/source.js
@@ -1,4 +1,4 @@
-const META = (demo) => [
+const META = (demo, demoOptions) => [
   {
     type: 'HEADING',
     render: 'Button',
@@ -16,9 +16,79 @@ const META = (demo) => [
     render: 'Demo',
   },
   {
-    type: 'EXAMPLE',
+    type: 'CONTENT',
     render: demo()
+  },
+  {
+    type: 'EXAMPLE',
+    render: demoOptions()
   }
 ];
+
+const OPTION_TYPE_BUTTON = [
+  {
+    id: 'type-primary',
+    label: 'BASIC',
+    value: 'BASIC',
+  },
+  {
+    id: 'type-primary',
+    label: 'DEFAULT',
+    value: 'DEFAULT',
+  },
+  {
+    id: 'type-primary',
+    label: 'PRIMARY',
+    value: 'PRIMARY',
+  },
+  {
+    id: 'type-primary',
+    label: 'SUCCESS',
+    value: 'SUCCESS',
+  },
+  {
+    id: 'type-primary',
+    label: 'INFO',
+    value: 'INFO',
+  },
+  {
+    id: 'type-primary',
+    label: 'WARNING',
+    value: 'WARNING',
+  },
+]
+
+const OPTION_SIZE_BUTTON = [
+  {
+    id: 'size-normal',
+    label: 'NORMAL',
+    value: 'NORMAL',
+  },
+  {
+    id: 'type-extra-large',
+    label: 'EXTRA-LARGE',
+    value: 'EXTRA-LARGE',
+  },
+  {
+    id: 'size-large',
+    label: 'LARGE',
+    value: 'LARGE',
+  },
+  {
+    id: 'size-small',
+    label: 'SMALL',
+    value: 'SMALL',
+  },
+  {
+    id: 'size-extra-small',
+    label: 'EXTRA-SMALL',
+    value: 'EXTRA-SMALL',
+  }
+]
+
+export {
+  OPTION_TYPE_BUTTON,
+  OPTION_SIZE_BUTTON
+}
 
 export default META;
